3938920c886b4cdd56122c5813435c6072fdebb9
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-01 21:06:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,10 +15,11 @@ def third_challenge
        :heroine => {:name "Juliet", :age "15", :status "alive"},
        :heroine_friends => [
            {:name "Steven", :age "30", :attitude "confused"},
-           {:name "Nurse", :age "44", :attitude "worried" }
+           {:name "Nurse", :age "44", :attitude "worried"}
        ]
     }
  }
 
+epic_tragedy[:montague][:heroine][:status] = "dead"
 
 end
